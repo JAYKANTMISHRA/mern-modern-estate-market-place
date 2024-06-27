f73bedac5b2632151a33e74a3df46533cde39523
jaykant <jaykantmishra88@gmail.com>
2024-06-27 12:05:10 +0530

    complete listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,10 +17,11 @@ export default function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
+        <Route path='/listing/:listingId' element={<Listing />} />
         <Route element={<PrivateRoute />}>
         <Route path='/profile' element={<Profile />} />
         <Route path='/create-listing' element={<CreateListing />} />
-        <Route path='/listing/:listingId' element={<Listing />} />
+        
         <Route
             path='/update-listing/:listingId'
             element={<UpdateListing />}
