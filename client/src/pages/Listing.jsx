79eec328c99675b8dc89372e4f4bce2289f49ba6
jaykant <jaykantmishra88@gmail.com>
2024-06-28 10:54:02 +0530

    create listing item components

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -103,12 +103,13 @@ export default function Listing() {
               Link copied!
             </p>
           )}
+
           <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
             <p className='text-2xl font-semibold'>
-              {listing.name} - ${' '}
+              {listing.name} - Rupees{' '}
               {listing.offer
-                ? listing.discountPrice.toLocaleString('en-US')
-                : listing.regularPrice.toLocaleString('en-US')}
+                ? listing.discountPrice
+                : listing.regularPrice}
               {listing.type === 'rent' && ' / month'}
             </p>
             <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
@@ -121,9 +122,11 @@ export default function Listing() {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountPrice} OFF
+                  Rupees {+listing.regularPrice - +listing.discountPrice} OFF
                 </p>
               )}
+
+
             </div>
             <p className='text-slate-800'>
               <span className='font-semibold text-black'>Description - </span>
